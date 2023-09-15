d3a317abbf64595862d96d738164105e73fe1e9c
danielOuattara <daniel.ouattara@gmx.com>
2023-09-15 23:13:02 +0200

    08 View components

---
+++ 08_View_Component/App.jsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightblue" }}
+      ></View>
+      <View
+        style={{ width: 200, height: 200, backgroundColor: "lightgreen" }}
+      ></View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
