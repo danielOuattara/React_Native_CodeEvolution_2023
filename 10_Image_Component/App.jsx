cf7625a809d3f9f93e4e782913dd5bfe3dd88118
danielOuattara <daniel.ouattara@gmx.com>
2023-09-15 23:13:32 +0200

    10 Image component

---
+++ 10_Image_Component/App.jsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ImageBackground } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
+      {/* <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      /> */}
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "plum",
+    padding: 40,
+  },
+});
