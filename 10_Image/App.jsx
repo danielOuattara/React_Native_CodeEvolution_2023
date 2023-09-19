458fdcfdcbf45d7f24412c06308ece7902e85d28
danielOuattara <daniel.ouattara@gmx.com>
2023-09-19 10:04:23 +0200

    <10 Image component

--- 10_Image/App.jsx
+++ 10_Image/App.jsx
@@ -6,11 +6,16 @@ const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
     <View style={styles.container}>
+      {/* Local image */}
       {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
+
+      {/* remote image */}
       {/* <Image
         source={{ uri: "https://picsum.photos/300" }}
         style={{ width: 300, height: 300 }}
       /> */}
+
+      {/* background image */}
       <ImageBackground source={logoImg} style={{ flex: 1 }}>
         <Text>IMAGE TEXT</Text>
       </ImageBackground>
