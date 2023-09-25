663da06b3186e6f501d749bc5385e5a889bc4ef9
danielOuattara <daniel.ouattara@gmx.com>
2023-09-25 21:52:38 +0200

    28 flex direction

---
+++ 28_flex_direction/App.jsx
@@ -0,0 +1,32 @@
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
+    <View style={styles.container}>
+      <Box style={{ backgroundColor: "#8e9b00" }}>box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>box 3</Box>
+      {/* <Box style={{ backgroundColor: "#ab9156" }}>box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>box 7</Box> */}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    flexDirection: "column-reverse",
+    flexDirection: "row",
+    flexDirection: "row-reverse",
+    marginTop: 64,
+    borderWidth: 4,
+    borderColor: "red",
+  },
+});
