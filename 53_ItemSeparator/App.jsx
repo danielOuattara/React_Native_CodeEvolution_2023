bda3796063211c7f2cffe9face8df7f13c5e1784
danielOuattara <daniel.ouattara@gmx.com>
2023-10-17 23:57:34 +0200

    53 ItemSeparator

---
+++ 53_ItemSeparator/App.jsx
@@ -0,0 +1,66 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import pokemonList from "./data.json";
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={pokemonList}
+        renderItem={({ item, index }) => {
+          console.log(item.id);
+          return (
+            <View key={index} style={styles.card}>
+              <Text style={styles.cardText}>{item.type}</Text>
+              <Text style={styles.cardText}>{item.name}</Text>
+            </View>
+          );
+        }}
+        // horizontal
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={(item) => {
+          console.log(item);
+          <View
+            style={{
+              height: 16,
+              marginHorizontal: 26,
+            }}
+          />;
+        }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#dfdfdf",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginHorizontal: 26,
+  },
+  cardText: {
+    fontSize: 22,
+  },
+});
+
+/*  
+NOTE: Rendering a list using map() method and ScrollView 
+      component is not the optimized technique 
+*/
