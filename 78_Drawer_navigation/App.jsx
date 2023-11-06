0fa957118a32bfcc08e6bed0a7e7e853a635c24e
danielOuattara <daniel.ouattara@gmx.com>
2023-11-06 15:18:47 +0100

    78 Drawer navigation

---
+++ 78_Drawer_navigation/App.jsx
@@ -0,0 +1,19 @@
+import { StatusBar } from "expo-status-bar";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import "react-native-gesture-handler";
+import { Home, About, Dashboard, Settings } from "./screens/index";
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={Dashboard} />
+        <Drawer.Screen name="Settings" component={Settings} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
